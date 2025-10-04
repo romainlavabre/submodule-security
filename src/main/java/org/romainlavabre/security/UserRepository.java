f364929d3cda80f3cfe7d646f9bcb1f5c46036e6
romainlavabre <romainlavabre98@gmail.com>
2025-10-04 10:50:19 +0200

    [2.1.1] Fix User.forgotPasswordCode

--- src/main/java/org/romainlavabre/security/UserRepository.java
+++ src/main/java/org/romainlavabre/security/UserRepository.java
@@ -2,10 +2,16 @@ package org.romainlavabre.security;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Romain Lavabre <romainlavabre98@gmail.com>
  */
 public interface UserRepository extends JpaRepository< User, Long > {
 
+    Optional<User> findById( Long id);
+
     User findByUsername( String username );
+
+    User findByForgotPasswordCode(String code);
 }
